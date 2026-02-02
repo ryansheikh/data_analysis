5b20793ad781437f36dc398a0d71482465cd64ca
ryansheikh <rayyansaleemsheikh@gmail.com>
2026-02-02 17:10:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,10 @@ st.set_page_config(
 # ----------------------------
 # Helpers
 # ----------------------------
-DATA_DIR_DEFAULT = os.path.join("data", "cleaned")
+DATA_DIR_DEFAULT = os.path.join("data_analysis", "data", "cleaned")
+
+
+
 
 FILE_MAP = {
     "monthly_sales": "cleaned_monthly_sales.csv",
@@ -425,3 +428,4 @@ elif page == "Pricing":
     fig2 = px.scatter(df2, x="AvgSellingPrice", y="TotalUnits", hover_data=["ProductName"])
     fig2.update_layout(height=420, margin=dict(l=10, r=10, t=30, b=10))
     st.plotly_chart(fig2, use_container_width=True)
+
